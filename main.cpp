6a965d2d44ff02d80339742e9952b6fcb962a657
dezsibalint <144552335+dezsibalint@users.noreply.github.com>
2024-04-26 09:43:45 +0200

    Dmemtrace nemjo,csinositgatas

--- main.cpp
+++ main.cpp
@@ -12,53 +12,57 @@
 int main() {
 ///Teszt A
     //Forrás tesztjei
+    //Később is kell majd az a forrás ezért teszten kívűl hozzuk létre.
     forras a(1);
-
-
     TEST(forras,create){
-
-            EXPECT_STREQ("Forras",a.getnev());
-            ///Kimeneten a bemenet van?
-            EXPECT_TRUE(a.out().getval());
+            EXPECT_STREQ("Forras",a.getnev()); //Létrejött-e az objektum?,és a neve helyes?
+            EXPECT_TRUE(a.out().getval()); //A beállított érték van a kimeneten?
     }ENDM
+
     TEST(forras,exeption){
-           EXPECT_THROW(forras b('c'),std::invalid_argument);
+           EXPECT_THROW(forras b('c'),std::invalid_argument&); //Érvénytelen bemenet tesztje
     }ENDM
-    vezetek v;
+
     //Vezeték tesztjei
+    ///A vezetéket is fogjuk használni később ezért teszten kívűl hozzuk létre.
+    vezetek v;
     TEST(vezetek,create){
-
-            EXPECT_STREQ("Vezetek",v.getnev());
-
-
+        EXPECT_STREQ("Vezetek",v.getnev()); //Létrejött-e az objektum?,és a neve helyes?
     }ENDM
+
     TEST(vezetek,connect){
             v.connect(a.out());//Forrás kimenetét csatlakoztatjuk a vezeték bemenetéhez.
             EXPECT_TRUE(v.getout().getval());//Vezeték kimenetén lévő állapot az e mint a forrás állapota?
     }ENDM
+
     //Inverter tesztjei
+    ///Az invertert is fogjuk használni később ezért teszten kívűl hozzuk létre.
     invereter i;
     TEST(invereter,create){
-            EXPECT_STREQ("Inverter",i.getnev());
+            EXPECT_STREQ("Inverter",i.getnev()); //Létrejött-e az objektum?,és a neve helyes?
     }ENDM
 
     TEST(invereter,connect){
-        i.connect(v.out());
+        i.connect(v.out()); //Vezeték kimenetét kötjük az inverter bemenetére.
         EXPECT_FALSE(i.out().getval());//Negált eredmény van a kimeneten?
     }ENDM
 
     //Norgate tesztjei
+    ///A norgate-t is fogjuk használni később ezért teszten kívűl hozzuk létre.
     norgate n(2);
     TEST(norgate,create){
-            EXPECT_STREQ("Norgate",n.getnev());
+            EXPECT_STREQ("Norgate",n.getnev()); //Létrejött-e az objektum?,és a neve helyes?
 
     }ENDM
     TEST(norgate,connect){
-     n.connect(i.out(),0);//0. lábra a vezeték kimenetét kötöm(1)
-     n.connect(v.out(),1);//1. lábra az inverter kimenetét (0)
-            EXPECT_FALSE(n.out().getval());///Nemvagy kapcsolat értelmében a kimenet állapota 0 kell,hogy legyen.
+     n.connect(i.out(),0); //0. lábra a vezeték kimenetét kötöm(1)
+     n.connect(v.out(),1); //1. lábra az inverter kimenetét (0)
+            EXPECT_FALSE(n.out().getval()); //Nemvagy kapcsolat értelmében a kimenet állapota 0 kell,hogy legyen.
 
     }ENDM
+    //*********************************************************************************************************************//
+                                        //Későbbi tesztek még nincsenek készen.//
+    // *********************************************************************************************************************//
     //Hálózati tároló tesztjei
    /* halozatitarolo test[4];
     TEST(halozatitarolo,betesz){
@@ -83,7 +87,7 @@ int main() {
     }ENDM
 ///TesztB
 //Teszthélózat tesztjei.
-/*teszthalozat th;
+    teszthalozat th;
     TEST(teszthalozat,felepit){
             EXPECT_NO_THROW(th.felepit()); //Hiba nélkül felépül-e?
 
